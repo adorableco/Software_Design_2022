6e997d0ecabd856fbef36432441e6116cdcb076e
뭄 <111948424+mchaewon@users.noreply.github.com>
2022-12-03 20:11:36 +0900

    helper gui modify

--- src/gui_package/Reservation_helper_GUI.java
+++ src/gui_package/Reservation_helper_GUI.java
@@ -90,12 +90,12 @@ public class Reservation_helper_GUI extends JFrame {
 			
 	}
 		
-	public Reservation_helper_GUI() {
+	public Reservation_helper_GUI(Reservation reservation_Info,File Selected_File) {
 		res=new Reservation();
 		resDB=new ReservationDB();
-		//LocalDate resvDate = reservation_Info.Get_Use_Day();
-		//LocalTime resvTime = reservation_Info.Get_Use_Time();
-		//String resvService = reservation_Info.Get_Use_Service();
+		LocalDate resvDate = reservation_Info.Get_Use_Day();
+		LocalTime resvTime = reservation_Info.Get_Use_Time();
+		String resvService = reservation_Info.Get_Use_Service();
 		
 		
 		createFrame("도우미 예약");
@@ -391,7 +391,356 @@ public class Reservation_helper_GUI extends JFrame {
 					res.setTime(time);
 					res.setService(service_choose);
 					res.setCost(cost);
-					resDB.dataUpload(res);
+					resDB.saveFile(res, Selected_File);
+					
+					dispose();
+				}					
+				else if(n.getText().equals("병원 예약")) {
+					JOptionPane.showMessageDialog(null,"병원을 예약하시겠습니까?");
+					//병원 예약창으로 넘어감
+					new AnimalHospitalReservationGUI("병원 예약 서비스");
+					dispose();
+				}
+				else if(n.getText().equals("미용샵 예약")) {
+					JOptionPane.showMessageDialog(null,"반려동물 미용샵을 예약하시겠습니까?");
+					//미용샵 예약창으로 넘어감
+					new PetGroomingSalonReservationGUI("반려동물 미용실 예약 서비스");
+					dispose();
+				}
+			}
+			
+		});
+		
+		//뒤로가기 버튼
+		BackBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane confirm = new JOptionPane();
+				int result;
+				result = confirm.showConfirmDialog(null, "돌아가시겠습니까? ", "돌아가기", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+				if(result == 0) {
+						dispose();
+				} 
+			}
+		});
+		
+		back_next_panel.add(next_btn);
+		
+		b_n_e_panel.add(explan_label);
+		b_n_e_panel.add(back_next_panel);
+		out_info_panel.add(info_panel);
+		
+		
+		super.setLayout(new BorderLayout(20,30));
+		add(listPanel, BorderLayout.WEST);
+		add(out_info_panel, BorderLayout.CENTER);
+		add(b_n_e_panel, BorderLayout.SOUTH);
+		
+		this.setVisible(true);
+		this.setFocusable(true);
+	
+	
+	}
+	
+	public Reservation_helper_GUI() {
+		res=new Reservation();
+		resDB=new ReservationDB();
+		
+		createFrame("도우미 예약");
+	
+		LineBorder line = new LineBorder(Color.black,1);
+		back_next_panel.setBorder(line);
+		
+		menu_btn.setFixedCellHeight(25);
+		menu_btn.setFixedCellWidth(140);
+		menu_btn.setSelectedIndex(0);
+
+		back_next_panel.setPreferredSize(new Dimension(600,47));
+		back_next_panel.add(BackBtn);
+		
+		explan_label.setOpaque(true);
+		explan_label.setBackground(Color.LIGHT_GRAY);
+		explan_label.setFont(new Font("Serief", Font.BOLD, 15));
+		
+		
+		listPanel.add(menu_btn);
+		
+		JLabel reservation_font = new JLabel("도우미 예약");
+		reservation_font.setFont(new Font("맑은 고딕", Font.BOLD, 40));
+		reservation_font.setForeground(new Color(5, 0, 153));
+		
+		// 컴포넌트를 title 컨테이너에 올림.
+		info_panel = new JPanel();
+		info_panel.setOpaque(true);
+		info_panel.setBackground(Color.gray);
+		info_panel.setForeground(Color.white);
+		info_panel.setOpaque(true);
+		info_panel.setFont(new Font("Serief", Font.BOLD, 15));
+		info_panel.setLayout(new GridLayout(4, 2));
+		//info_panel.setVisible(true);
+		
+		//도우미 검색
+		dim1=new Dimension(230,30);
+		JPanel helper_search_panel_1 = new JPanel(new FlowLayout(FlowLayout.RIGHT,50,30));
+		JLabel helper_search_label = new JLabel("도우미 검색 : ", JLabel.CENTER);
+		helper_search_label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		helper_search_label.setForeground(Color.white);
+		helper_search_panel_1.setOpaque(true);
+		helper_search_panel_1.setBackground(Color.gray);
+		helper_search_panel_1.setOpaque(true);
+		helper_search_panel_1.add(helper_search_label);
+		
+		JPanel helper_search_panel_2 = new JPanel(new FlowLayout(FlowLayout.LEFT,10,30));
+		helper_search_button = new JButton("도우미 검색");
+		helper_search_button.setFont(new Font("맑은 고딕", Font.BOLD, 17));
+		helper_search_panel_2.setOpaque(true);
+		helper_search_panel_2.setBackground(Color.gray);
+		helper_search_panel_2.setOpaque(true);
+		helper_search_button.setPreferredSize(dim1);
+			
+		helper_search_panel_2.add(helper_search_button);
+		
+		info_panel.add(helper_search_panel_1); 
+		info_panel.add(helper_search_panel_2);
+		
+		//이용날짜
+		dim2=new Dimension(70,30);
+		JPanel date_panel_1 = new JPanel(new FlowLayout(FlowLayout.RIGHT,50,30));
+		JLabel date_label = new JLabel("이용 날짜 : ", JLabel.CENTER);
+		date_label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		date_label.setForeground(Color.white);
+		date_panel_1.setOpaque(true);
+		date_panel_1.setBackground(Color.gray);
+		date_panel_1.setOpaque(true);
+		date_panel_1.add(date_label);
+			
+		JPanel date_panel_2 = new JPanel(new FlowLayout(FlowLayout.LEFT,10,32));
+		date_panel_2.setOpaque(true);
+		date_panel_2.setBackground(Color.gray);
+		date_panel_2.setOpaque(true);
+		yearCombo = new JComboBox<String>(year);
+		monthCombo = new JComboBox<String>(month);
+		dayCombo = new JComboBox<String>(day);
+		yearCombo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		monthCombo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		dayCombo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		yearCombo.setPreferredSize(dim2);
+		monthCombo.setPreferredSize(dim2);
+		dayCombo.setPreferredSize(dim2);
+				
+		date_panel_2.add(yearCombo);
+		date_panel_2.add(monthCombo);
+		date_panel_2.add(dayCombo);
+		
+		info_panel.add(date_panel_1); 
+		info_panel.add(date_panel_2);
+		
+		//이용시간
+		dim3=new Dimension(110,30);
+		JPanel time_panel_1 = new JPanel(new FlowLayout(FlowLayout.RIGHT,50,30));
+		JLabel time_label = new JLabel("이용 시간 : ", JLabel.CENTER);
+		time_label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		time_label.setForeground(Color.white);
+		time_panel_1.setOpaque(true);
+		time_panel_1.setBackground(Color.gray);
+		time_panel_1.setOpaque(true);
+		time_panel_1.add(time_label);
+					
+		JPanel time_panel_2 = new JPanel(new FlowLayout(FlowLayout.LEFT,10,32));
+		hourCombo = new JComboBox<String>(hour);
+		minCombo = new JComboBox<String>(min);
+		hourCombo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		minCombo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		time_panel_2.setOpaque(true);
+		time_panel_2.setBackground(Color.gray);
+		time_panel_2.setOpaque(true);
+		hourCombo.setPreferredSize(dim3);
+		minCombo.setPreferredSize(dim3);
+						
+		time_panel_2.add(hourCombo);
+		time_panel_2.add(minCombo);
+				
+		info_panel.add(time_panel_1); 
+		info_panel.add(time_panel_2);
+		
+		//이용서비스
+		dim1=new Dimension(230,30);
+		JPanel service_panel_1 = new JPanel(new FlowLayout(FlowLayout.RIGHT,50,30));
+		JLabel service_label = new JLabel("이용 서비스 : ", JLabel.CENTER);
+		service_label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		service_label.setForeground(Color.white);
+		service_panel_1.setOpaque(true);
+		service_panel_1.setBackground(Color.gray);
+		service_panel_1.setOpaque(true);
+		service_panel_1.add(service_label);
+					
+		JPanel service_panel_2 = new JPanel(new FlowLayout(FlowLayout.LEFT,10,32));
+		serviceCombo = new JComboBox<String>(service);
+		serviceCombo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		service_panel_2.setOpaque(true);
+		service_panel_2.setBackground(Color.gray);
+		service_panel_2.setOpaque(true);
+		serviceCombo.setPreferredSize(dim1);
+		service_panel_2.add(serviceCombo);
+		
+		info_panel.add(service_panel_1); 
+		info_panel.add(service_panel_2);		
+		
+		// 이벤트 처리
+		//메뉴에서 도우미예약 선택
+		menu_btn.addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				int selection = menu_btn.getSelectedIndex();
+				switch(selection) {
+				case(0):
+					explan_label.setText("도우미 예약");
+					out_info_panel.add(info_panel);
+					break;
+				}
+			}
+		});
+		//도우미 검색 Action
+		helper_search_button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//도우미 검색 창으로 넘어감
+				new PetSitterSearchGUI("도우미 검색 서비스");
+			}
+		});
+		
+		//이용 날짜 Action
+		yearCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox y=(JComboBox)e.getSource();
+				if(y.getSelectedItem().equals("yyyy"))
+					JOptionPane.showMessageDialog(null,"잘못선택하였습니다.");
+				else {
+					ye=y.getSelectedItem().toString();
+					//y.setEnabled(false);
+				}
+			}
+		});
+		monthCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox m=(JComboBox)e.getSource();
+				if(m.getSelectedItem().equals("MM"))
+					JOptionPane.showMessageDialog(null,"잘못선택하였습니다.");
+				else {
+					mo=m.getSelectedItem().toString();
+					//m.setEnabled(false);
+				}
+			}
+			
+		});
+		dayCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox d=(JComboBox)e.getSource();
+				if(d.getSelectedItem().equals("dd"))
+					JOptionPane.showMessageDialog(null,"잘못선택하였습니다.");
+				else {
+					da=d.getSelectedItem().toString();
+				}
+			}
+			
+		});
+		
+		//이용 시간 Action  몇시간하는지도 받아야함
+		hourCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox h=(JComboBox)e.getSource();
+				if(h.getSelectedItem().equals("HH"))
+					JOptionPane.showMessageDialog(null,"잘못선택하였습니다.");
+				else {
+					ho=h.getSelectedItem().toString();
+				}
+			}
+			
+		});
+		minCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox m=(JComboBox)e.getSource();
+				if(m.getSelectedItem().equals("mm"))
+					JOptionPane.showMessageDialog(null,"잘못선택하였습니다.");
+				else {
+					mi=m.getSelectedItem().toString();
+				}
+			}
+			
+		});
+		
+		//이용 서비스 Action
+		serviceCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox meau = (JComboBox)e.getSource();
+				if(meau.getSelectedItem().equals("서비스 선택"))
+					JOptionPane.showMessageDialog(null,"잘못선택하였습니다.");
+				
+				service_choose = meau.getSelectedItem().toString();
+				
+				if(service_choose.equals("산책")||service_choose.equals("집 방문")) {
+					next_btn.setText("결제");
+				}
+					
+				else if (service_choose.equals("병원 동행")) {
+					next_btn.setText("병원 예약");
+				}
+					
+				else if(service_choose.equals("반려동물 미용샵 동행")) {
+					next_btn.setText("미용샵 예약");
+				}
+			}
+		});
+		
+		next_btn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JButton n=(JButton)e.getSource();
+				//서비스콤보가 "산책","집방문"이면 결제창으로 넘어감
+				//넘어가기전에"결제하시겠습니까" 메세지창 띄우기 
+				//서비스콤보가 "병원 동행","미용샵 동행"이면 각각의 예약 창으로 넘어감
+				
+				//지금 짠 코드는 결제 안하고 바로 예약 DB에 저장함
+				//예약 성공하면 메인창으로 넘어감
+				if(n.getText().equals("결제")) {
+					cost=10000;
+					JOptionPane.showMessageDialog(null,cost+"원 결제되었습니다.");  //결제창 넘어감??
+					//reservation 클래스에 정보줌
+					int i_ye=Integer.parseInt(ye);
+					int i_mo=Integer.parseInt(mo);
+					int i_da=Integer.parseInt(da);
+					int i_ho=Integer.parseInt(ho);
+					int i_mi=Integer.parseInt(mi);
+					LocalDate date=LocalDate.of(i_ye, i_mo, i_da);
+					LocalTime time=LocalTime.of(i_ho, i_mi);
+					res.setDate(date);
+					res.setTime(time);
+					res.setService(service_choose);
+					res.setCost(cost);
+					resDB.saveFile(res);
 					
 					dispose();
 				}					
